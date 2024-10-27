44eb1ef12777c0293c2221a6120cd5ec12a3baf2
godfryope <elolengodfrey@gmail.com>
2024-10-27 14:45:08 +0100

    new

--- whale_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WhaleAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'whale_app'
--- whale_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [     
-    path('', TapCoinView.as_view(), name='tap_coin'),
-    path('telegram-auth/', telegram_auth, name='telegram_auth'),  # Endpoint to handle Telegram auth
-    path('claim-reward/', ClaimRewardView.as_view(), name='claim_reward'),
-    path('register-wallet/', RegisterWalletView.as_view(), name='register_wallet'),
-]
